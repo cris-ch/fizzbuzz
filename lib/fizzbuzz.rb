5c251aef2a694c91e937f171407db5287dc4ff38
foolfighter <cristian.christensen@gmail.com>
2021-11-30 15:04:42 +0100

    Completed Fizzbuzz & RSpec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,22 @@
+=begin
+FizzBuzz is a simple coding challenge, often described as a code kata. The objective of FizzBuzz is to create a program with the following specification:
+
+The program can be passed a number.
+When passed a number that is a multiple of 3, the program returns the message 'Fizz'.
+When passed a number that is a multiple of 5, the program returns the message 'Buzz'.
+When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and returns the message 'FizzBuzz'.
+In all other cases, the program simply returns the given number.
+
+=end
+
 def fizzbuzz(number)
-  if number % 3 == 0
-    return "Fizz"
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
   elsif number % 5 == 0
-    return "Buzz"
+    return "buzz"
+  elsif number % 3 == 0
+    return "fizz"
+  else 
+    return number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,17 @@ describe "fizzbuzz" do
   it 'returns "buzz" when 5' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
+
+  it 'returns "fizzbuzz" when 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns 53 when 53' do
+    expect(fizzbuzz(53)).to eq 53
+  end
+
+  it 'returns 143 when 143' do
+    expect(fizzbuzz(143)).to eq 143
+  end
+
 end
